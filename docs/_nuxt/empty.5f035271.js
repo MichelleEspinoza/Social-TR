d614087e9828fd15f088b14fff6d06607862d613
MichelleEspinoza <michelle@socalstudio.com>
2023-04-19 12:54:04 -0700

    add static files

---
+++ docs/_nuxt/empty.5f035271.js
@@ -0,0 +1,1 @@
+import{f as t,b as o,z as r,o as n}from"./entry.0dc10e3b.js";const s={};function c(e,a){return n(),o("div",null,[r(e.$slots,"default")])}const f=t(s,[["render",c]]);export{f as default};
