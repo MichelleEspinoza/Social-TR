7f2eb7802992fed72a9c4ac7b6b1208acbe8894c
MichelleEspinoza <memichelle473@gmail.com>
2023-04-19 20:01:52 +0000

    Deploying to gh-pages from @ MichelleEspinoza/social-trader@61be742c00a50d05b6b5387f2e51628af47032ca 🚀

---
+++ _nuxt/composables.4fff630f.js
@@ -0,0 +1,1 @@
+import{D as r}from"./entry.8a54ea1d.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
---
+++ _nuxt/empty.3e68f1db.js
@@ -0,0 +1,1 @@
+import{f as t,b as o,z as r,o as n}from"./entry.8a54ea1d.js";const s={};function c(e,a){return n(),o("div",null,[r(e.$slots,"default")])}const f=t(s,[["render",c]]);export{f as default};
